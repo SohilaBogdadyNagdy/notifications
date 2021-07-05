7943c0b1c2b93c95b32503f57e281fc013f8dec9
sohilaBogdadyNagdy <sohila.bogdady2012@gmail.com>
2021-07-05 23:37:42 +0200

    Add mysql images to docker compose services

--- app.py
+++ app.py
@@ -5,18 +5,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return {}
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-
-@app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+@app.route('/send')
+def send_notification():
+    return 'Hello World! I have been seen times.\n'
