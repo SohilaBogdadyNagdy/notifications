aa9b01e4203a7f616f607946990ba8a4c0ea4bcd
sohilaBogdadyNagdy <sohila.bogdady2012@gmail.com>
2021-07-09 00:01:46 +0200

    Add new user api and send notifcation api

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 from flask import Flask, request, jsonify
 
 from db import initialize_db
-from models import Notification
+from models import Notification, User
 
 app = Flask(__name__)
 
@@ -12,20 +12,45 @@ initialize_db(app)
 @app.route('/notifications')
 def list_notification():
     data = Notification.objects.all().to_json()
-    return jsonify({'notifications': data}), 200 
+    return jsonify({'data': data}), 200 
 
 @app.route('/notifications/:id')
 def get_notification():
     data = Notification.objects.filter(id=id).to_json()
-    return jsonify({'notifications': data}), 200
+    return jsonify({'data': data}), 200
 
 @app.route('/notification', methods=['POST'])
 def post_notification():
     try:
         data = json.loads(request.data)
         notification = Notification(**data).save()
-        return jsonify({'notification': notification}), 201
+        return jsonify({'data': notification}), 201
     except BaseException as e:
         print(e)
         return e.message, 400
+
+
+@app.route('/users', methods=['POST'])
+def post_user():
+    try:
+        data = json.loads(request.data)
+        user = User(**data).save()
+        return jsonify({'data': user}), 201
+    except BaseException as e:
+        print(e)
+        return e.message, 400
+
+
+@app.route('/send/:notificationId', methods=['POST'])
+def send():
+    try:
+        userIds = json.loads(request.data)
+        return jsonify({
+            "message": "Send Successfully",
+            "success": True
+        }), 200
+    except BaseException as e:
+        print(e)
+        return e.message, 400
+
 
